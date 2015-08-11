3c41d78e4c447a421a256aa93e76313837ac5883
Pavel Statkevich <statpavel@gmail.com>
2015-08-11 11:27:33 +0300

    First version

---
+++ sources/ApplicationFileProject/src/main/java/test/app/form/ApplicationForm.java
@@ -0,0 +1,46 @@
+package test.app.form;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import org.sqlite.SQLiteConfig;
+
+public class Applicationform
+{
+
+    private static final Logger LOGGER = Logger.getLogger(ApplicationForm.class.getName());
+
+    public static void main(final String[] args)
+    {
+        System.out.println("\r\nFill in the following application form Anketa_Ericpol_Brest_PA4.docx");
+        System.out.println("and send it to this email address: " + getEmailAddress());
+    }
+
+    private static String getEmailAddress()
+    {
+        LOGGER.finest("getEmailAddress");
+        String emailAddress = "";
+        try
+        {
+            SQLiteConfig config = new SQLiteConfig();
+            config.setReadOnly(true);
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite::resource:project_db", config.toProperties());
+            Statement sta = conn.createStatement();
+            ResultSet res = sta.executeQuery("SELECT mail FROM contacts WHERE id=1");
+            emailAddress = res.getString("email");
+            res.close();
+            sta.close();
+            conn.close();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return emailAddress;
+    }
+
+}
